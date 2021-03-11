e5830aea29cd571f1f2f523fec45b9a9722f08a5
Kostas <kvstaw4@hotmail.com>
2021-03-11 13:33:40 +0200

    fs

--- feedback_form/send_mail.php
+++ feedback_form/send_mail.php
@@ -9,7 +9,7 @@ $webmaster_email = "kvstaw4@hotmail.com";
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
-// $feedback_page = "../index.html";
+$feedback_page = "../index.html";
 $error_page = "error_message.html";
 $thankyou_page = "thank_you.html";
 
@@ -17,9 +17,9 @@ $thankyou_page = "thank_you.html";
 This next bit loads the form field data into variables.
 If you add a form field, you will need to add it here.
 */
-$email_address = $_REQUEST['email'] ;
+$email_address = $_REQUEST['email_address'] ;
 $comments = $_REQUEST['comments'] ;
-$first_name = $_REQUEST['name'] ;
+$first_name = $_REQUEST['first_name'] ;
 $msg = 
 "First Name: " . $first_name . "\r\n" . 
 "Email: " . $email_address . "\r\n" . 
@@ -49,7 +49,7 @@ function isInjected($str) {
 }
 
 // If the user tries to access this script directly, redirect them to the feedback form,
-if (!isset($_REQUEST['email'])) {
+if (!isset($_REQUEST['email_address'])) {
 header( "Location: $feedback_page" );
 }
 
